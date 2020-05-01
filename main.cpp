7255bc7df15c54a9d0d9b1c85d91543e80bd51a8
Jisan129 <jishananam@iut-dhaka.edu>
2020-05-01 10:49:42 +0600

    It is Dynamic Algorithm to solve max-min problem. Coin change dp Real version

--- main.cpp
+++ main.cpp
@@ -1,44 +1,76 @@
 #include <iostream>
 #include <bits/stdc++.h>
-#include <wingdi.h>
+
 
 using namespace std;
 
-void countcoin(int array[], int number, int x) {
+long long countcoin(long long array[], long long number, long long x) {
 
-    int dp[x][number+1];
-    for(int i=0;i<x;i++){
+    long long dp[x][number+1];
+    for(long long i=0;i<x;i++){
         dp[i][0]=0;
     }
-    for(int i=0;i<x;i++){
-        for(int j=1;j<number+1;j++){
-            if(array[i]<j){
-                dp[i][j]=-1;
-            } else if(j-array[i]>=0&&dp[i][j-array[i]!=-1]){
-                dp[i][j]=dp[i][j-array[i]]+1;
-
-            } else if(dp[i][j-array[i]]==-1){
-                dp[i][j-array[i]]=-1;
+    for(long long i=0;i<x;i++){
+        for(long long j=1;j<=number;j++){
+            //cout<<array[i]<<" "<<j<<"No";
+            //cout<<dp[i][j-array[i]]<<" ";
+            if(array[i]>j){
+                if(i!=0){
+                    if(dp[i-1][j]==-1){
+                        dp[i][j]=-1;}
+                    else{
+                        dp[i][j]=dp[i-1][j];
+                    }} else{
+                    dp[i][j]=-1;
+                }
             }
-        }
-    }
-    for(int i=0;i<x;i++){
-        for(int j=0;j<number+1;j++){
-            cout<<dp[i][j]<<" ";
-        }
-        cout<<endl;
-    }
+            else if (dp[i][j-array[i]]==-1){
+                if(i!=0){
+                    if(dp[i-1][j]==-1){
+                        dp[i][j]=-1;}
+                    else{
+                        dp[i][j]=dp[i-1][j];
+                    }}
+                else{
+                    dp[i][j]=-1;
+                }
+            }
+            else if(j>=array[i]&&dp[i][j-array[i]]!=-1){
+                if(i!=0) {
+                    //cout<<"Here";
+                    long long excluding=dp[i-1][j];
+                    long long including=dp[i][j-array[i]]+1;
+                    if(excluding>including){
+                        dp[i][j]=excluding;
+                    } else{
+                        dp[i][j]=including;
+                    }
 
+                } else{
+                    dp[i][j] = dp[i][j - array[i]] + 1;
+                }
+            }
+
+        }
+        //cout<<endl;
+    }
+    /*   for(long long i=0;i<x;i++){
+           for(long long j=0;j<=number;j++){
+               cout<<dp[i][j]<<" ";
+           }
+           cout<<endl;
+       }*/
+    return dp[x-1][number];
 
 }
 
 int main(){
-    int number,x;
+    long long number,x;
     cin>>number>>x;
-    int array[number+1];
-    for(int i=0;i<x;i++){
+    long long array[number+1];
+    for(long long i=0;i<x;i++){
         cin>>array[i];
     }
-    countcoin(array,number,x);
+    cout<<countcoin(array,number,x);
+    return 0;
 }
-
