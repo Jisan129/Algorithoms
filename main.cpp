9c58fb7e6e057dc95debaa2732c852e3d99b82a7
Jisan129 <jishananam@iut-dhaka.edu>
2020-05-01 10:25:45 +0600

    It is Dynamic Algorithm to solve max-min problem. Coin change dp

--- main.cpp
+++ main.cpp
@@ -3,24 +3,42 @@
 #include <wingdi.h>
 
 using namespace std;
-int fibo[1000]={-1};
-int findFibonacci(int number) {
-  if(number<2){
-      return number;
-  }
-  if(fibo[number]!=-1){
-      return fibo[number];
-  }
-  else {
-      fibo[number]=findFibonacci(number-2)+findFibonacci(number-1);
-      return fibo[number];
-  }
+
+void countcoin(int array[], int number, int x) {
+
+    int dp[x][number+1];
+    for(int i=0;i<x;i++){
+        dp[i][0]=0;
+    }
+    for(int i=0;i<x;i++){
+        for(int j=1;j<number+1;j++){
+            if(array[i]<j){
+                dp[i][j]=-1;
+            } else if(j-array[i]>=0&&dp[i][j-array[i]!=-1]){
+                dp[i][j]=dp[i][j-array[i]]+1;
+
+            } else if(dp[i][j-array[i]]==-1){
+                dp[i][j-array[i]]=-1;
+            }
+        }
+    }
+    for(int i=0;i<x;i++){
+        for(int j=0;j<number+1;j++){
+            cout<<dp[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
 
 }
 
 int main(){
-    int number;
-    cin>>number;
-    int c=findFibonacci(number);
-    cout<<c;
+    int number,x;
+    cin>>number>>x;
+    int array[number+1];
+    for(int i=0;i<x;i++){
+        cin>>array[i];
+    }
+    countcoin(array,number,x);
 }
+
